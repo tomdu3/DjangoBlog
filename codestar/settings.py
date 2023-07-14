8ff0f3c6f4a9cf8437a36ece9376ef12502f74eb
Tomislav Dukez <tomdu3@ymail.com>
2023-07-14 13:45:30 +0000

    add allauth and account login functionality

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.environ.get('DEBUG', False)
 ALLOWED_HOSTS = ['django-blog-tom-03737538be6c.herokuapp.com',
                  os.environ.get('ALLOWED_HOSTS')]
 
-
+ACCOUNT_EMAIL_VERIFICATION = 'none' 
 
 # Application definition
 
@@ -40,12 +40,22 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
     'blog',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
