54c138f238b4c416075a3aa119d7f6afa11bc4ea
Tomislav Dukez <tomdu3@ymail.com>
2023-07-19 06:11:48 +0000

    add challenge message

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.views import generic, View
 from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
+from django.contrib import messages
 
 
 class PostList(generic.ListView):
@@ -50,6 +51,7 @@ class PostDetail(View):
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
+            messages.success(request, 'The comment is waiting for approval!')
         else:
             comment_form = CommentForm()
 
